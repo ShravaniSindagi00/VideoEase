abaef4f2295249e4e56433c754e8905918db3779
Shravani Sindagi <shravanisindagi00@gmail.com>
2025-01-01 21:11:20 +0530

    Updated mongoose connection

--- server.js
+++ server.js
@@ -9,3 +9,16 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config(); // Load environment variables from .env file
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
+
